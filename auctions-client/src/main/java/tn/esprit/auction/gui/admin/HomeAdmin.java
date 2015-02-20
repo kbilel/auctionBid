c937a8d78763ea8c8587f18f57e18af98538ad82
houda.baya <houda.baya@esprit.tn>
2015-02-20 14:32:14 +0100

    homeAdmin

---
+++ auctions-client/src/main/java/tn/esprit/auction/gui/admin/HomeAdmin.java
@@ -0,0 +1,20 @@
+package tn.esprit.auction.gui.admin;
+
+import javax.swing.JPanel;
+
+public class HomeAdmin extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public HomeAdmin() {
+		setBounds(0, 50, 1158, 850);
+		setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 1158, 850);
+		add(panel);
+
+	}
+
+}
