b2873d66809504d2bb0bc5fe183dedf5e8c372cf
bilel <bilel@192.168.8.100>
2015-03-30 23:05:16 +0200

    Signed-off-by: bilel <bilel@192.168.8.100>

--- auctions-client/src/test/java/Test.java
+++ auctions-client/src/test/java/Test.java
@@ -1,8 +1,21 @@
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+
+
+
+@SuppressWarnings("deprecation")
 public class Test {
 
-	public Test() {
-		// TODO Auto-generated constructor stub
+public Test() {
+	// TODO Auto-generated constructor stub
+}
+
+
 	}
 
-}
+
