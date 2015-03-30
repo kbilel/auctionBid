f08bd0b2feee50c9f5c14986894067de31594d89
bilel <bilel@192.168.8.100>
2015-03-30 23:09:55 +0200

    Signed-off-by: bilel <bilel@192.168.8.100>

--- auctions-client/src/test/java/Test.java
+++ auctions-client/src/test/java/Test.java
@@ -1,14 +1,7 @@
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
-import static org.junit.Assert.*;
-
-import org.junit.Assert;
 
 
 
-@SuppressWarnings("deprecation")
+
 public class Test {
 
 public Test() {
